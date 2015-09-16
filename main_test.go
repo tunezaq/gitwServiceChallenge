f7011667ee887a620ba44ceb7d23c8aaf7f49f67
Cody Powell <cody@tune.com>
2015-09-16 05:38:29 +0000

    Helpers and tests written for all endpoints. Now need to start combining them.

--- main_test.go
+++ main_test.go
@@ -6,7 +6,7 @@ import (
 	`fmt`
 	`io/ioutil`
 	`net/http`
-	//`strconv`
+	`strconv`
 	`testing`
 )
 
@@ -24,27 +24,110 @@ func TestChallenge1Post(t *testing.T) {
 	deleteAll(t)
 
 	// Populate a single key / value pair via a POST call.
-	cp1 := &CachePair{Key: "falfa", Value: "fbeta"}
+	cp1 := &CachePair{Key: "foo", Value: "bar"}
 
 	// Status code 201 returned with a link to new cache item's key.
 	post(t, cp1)
+}
 
-	cp2 := &CachePair{Key: 100, Value: "yay!"}
+func TestChallenge1GetAll(t *testing.T) {
+	// Start from a clean slate.
+	deleteAll(t)
+
+	// Populate multiple cache items via post calls.
+	cp1 := &CachePair{Key: "alpha", Value: "beta"}
+	post(t, cp1)
+
+	cp2 := &CachePair{Key: 100, Value: "dolla billllllllz!"}
 	post(t, cp2)
 
 	cp3 := &CachePair{Key: 100.001, Value: true}
 	post(t, cp3)
 
-	// Verifies that the cache now contains the cache pair we expect.
-	// getKey(t, cp)
-
+	// Get cache contents, compare to our in memory verison.
 	cpairs := []*CachePair{cp1, cp2, cp3}
 	getAll(t, cpairs)
+}
+
+func TestChallenge1DeleteAll(t *testing.T) {
+	// Start from a clean slate.
+	deleteAll(t)
+
+	// Populate multiple cache items via post calls.
+	cp1 := &CachePair{Key: "arugula", Value: false}
+	post(t, cp1)
+
+	cp2 := &CachePair{Key: -1, Value: -0.0005}
+	post(t, cp2)
+
+	// Delete everything. Dieeee, cache scum!
+	deleteAll(t)
+
+	// Cache should be empty now.
+	getAll(t, []*CachePair{})
+}
+
+func TestChallenge1GetKey(t *testing.T) {
+	// Populate a single cache item.
+	cp1 := &CachePair{Key: "bazooty", Value: "tootyfruity"}
+	post(t, cp1)
+
+	// Get the key we just populated.
+	getKey(t, cp1)
+}
+
+func TestChallenge1Put(t *testing.T) {
+	// Populate a single cache item.
+	cp1 := &CachePair{Key: 99, Value: 100}
+	post(t, cp1)
+
+	// Update the value via a put call.
+	cp1.Value = 101
+	putKey(t, cp1)
+
+	// Get the key we just populated.
+	getKey(t, cp1)
+}
+
+func TestChallenge1DeleteKey(t *testing.T) {
+	// Start from a clean slate.
+	deleteAll(t)
+
+	// Populate a couple items via post.
+	cp1 := &CachePair{Key: true, Value: "rainier"}
+	post(t, cp1)
+
+	cp2 := &CachePair{Key: "haggis", Value: 9999999.999}
+	post(t, cp2)
+
+	// Delete just that key. Other stuff should live.
+	deleteKey(t, cp1)
+
+	// Cache should be empty now.
+	getAll(t, []*CachePair{cp2})
 }
 
 func deleteAll(t *testing.T) {
 	expectedStatus := http.StatusNoContent
 	req, err := http.NewRequest("DELETE", LocalHost, nil)
+	if err != nil {
+		t.Errorf("Delete call failed: %s", err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Errorf("Error reading response body: %s", err)
+	}
+
+	if resp.StatusCode != expectedStatus {
+		t.Errorf("Response code of %s doesn't match expected %s.", resp.StatusCode, expectedStatus)
+	}
+}
+
+func deleteKey(t *testing.T, cp *CachePair) {
+	expectedStatus := http.StatusNoContent
+	endpoint := fmt.Sprintf("%s%s", LocalHost, getUrlFriendlyKey(cp))
+	req, err := http.NewRequest("DELETE", endpoint, nil)
 	if err != nil {
 		t.Errorf("Delete call failed: %s", err)
 	}
@@ -82,9 +165,29 @@ func post(t *testing.T, cp *CachePair) {
 	}
 }
 
+func putKey(t *testing.T, cp *CachePair) {
+	expectedStatus := http.StatusNoContent
+	endpoint := fmt.Sprintf("%s%s", LocalHost, getUrlFriendlyKey(cp))
+	cpJson, err := json.Marshal(cp)
+
+	req, err := http.NewRequest("PUT", endpoint, bytes.NewReader(cpJson))
+	if err != nil {
+		t.Errorf("Put call failed: %s", err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Errorf("Error reading response body: %s", err)
+	}
+
+	if resp.StatusCode != expectedStatus {
+		t.Errorf("Response code of %s doesn't match expected %s.", resp.StatusCode, expectedStatus)
+	}
+}
+
 func getKey(t *testing.T, cp *CachePair) {
 	expectedStatus := http.StatusOK
-	endpoint := fmt.Sprintf("%s%s", LocalHost, cp.Key)
+	endpoint := fmt.Sprintf("%s%s", LocalHost, getUrlFriendlyKey(cp))
 	resp, err := http.Get(endpoint)
 
 	if err != nil {
@@ -191,4 +294,21 @@ func compareCaches(t *testing.T, memoryPairs []*CachePair, serverPairs []CachePa
 			t.Errorf("Key %s found in memory cache, but not in server cache.", k)
 		}
 	}
+}
+
+func getUrlFriendlyKey(cp *CachePair) string {
+	if k_int, ok := cp.Key.(int); ok {
+		return strconv.Itoa(k_int)
+	}
+	if k_float, ok := cp.Key.(float64); ok {
+		return strconv.FormatFloat(k_float, 'f', 6, 64)
+	}
+	if k_bool, ok := cp.Key.(bool); ok {
+		if k_bool {
+			return "true"
+		} else {
+			return "false"
+		}
+	}
+	return fmt.Sprintf("%s", cp.Key)
 }
